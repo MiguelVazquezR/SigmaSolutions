34e6b48f38daf24038aa191bcab5772ce5ab1aef
Angel <angelvazquez470@gmail.com>
2023-03-16 19:38:53 -0600

    Index Landing page added

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,6 +8,8 @@
 
         <!-- Fonts -->
         <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="assets/css/animate.css" />
+        <link rel="stylesheet" href="assets/css/tailwind.css" />
 
         <!-- Styles -->
         <style>
